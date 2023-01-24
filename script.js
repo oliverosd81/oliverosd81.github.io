2a71cba2064e2cabe610781782a0c29378632b88
Daniel Eduardo Oliveros Fuenmayor <oliverosdf@gmail.com>
2023-01-24 10:50:47 +0100

    minor changes

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.hero nav ul');
-let info = document.querySelector('body');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
-}
-
-info.onload = () => {
-    alert('This site is still under construction, thanks for your visit!');
 }
