dc7a403108d7046394f0dc298b046989a020fc5a
Daniel Eduardo Oliveros Fuenmayor <oliverosdf@gmail.com>
2023-01-25 19:42:13 +0100

    minor bug fixed, still in process

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ menu.onclick = () => {
 
 // ANIMATIONS //
 // About me Section //
-// window.onscroll = () => {
-//   if (document.documentElement.scrollTop > 220) {
-//     document.getElementById("profile-pic").className = "fade-in-left";
-//     document.getElementById("main-about").className = "about-text fade-in-right";
-//   } else {
-//     document.getElementById("profile-pic").className = "hidden";
-//     document.getElementById("main-about").className = "hidden";
-//   }
-// }
+window.onscroll = () => {
+  if (document.documentElement.scrollTop > 230) {
+    document.getElementById("profile-pic").className = "fade-in-left";
+    document.getElementById("main-about").className = "about-text fade-in-right";
+  } else {
+    document.getElementById("profile-pic").className = "hidden";
+    document.getElementById("main-about").className = "hidden";
+  }
+}
 
 // Web Services //
 // window.onscroll = () => {
@@ -26,21 +26,21 @@ menu.onclick = () => {
 //     } else {
 //       document.getElementById("tittle-id").className = "hidden";
 //     }
-//   }
+// }
 
-window.onscroll = function() {animation()};
+// window.onscroll = function() {animation()};
 
-function animation() {
-    if (document.body.scrollTop > 230 || document.documentElement.scrollTop > 230) {
-        document.getElementById("profile-pic").className = "fade-in-left";
-        document.getElementById("main-about").className = "about-text fade-in-right";
+// function animation() {
+//     if (document.body.scrollTop > 230 || document.documentElement.scrollTop > 230) {
+//         document.getElementById("profile-pic").className = "fade-in-left";
+//         document.getElementById("main-about").className = "about-text fade-in-right";
 
-    } else if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
-        document.getElementById("tittle-id").className = "tittle scale-in-center";
-    }
+//     } else if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
+//         document.getElementById("tittle-id").className = "tittle scale-in-center";
+//     }
 
-    else {
-        document.getElementById("profile-pic").className = "hidden";
-        document.getElementById("main-about").className = "hidden";
-    }
-}
+//     else {
+//         document.getElementById("profile-pic").className = "hidden";
+//         document.getElementById("main-about").className = "hidden";
+//     }
+// }
