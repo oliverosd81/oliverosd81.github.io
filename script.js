4ecd249d37ff2974284c7758a45fcc0d85db1d17
Daniel Eduardo Oliveros Fuenmayor <oliverosdf@gmail.com>
2023-01-31 01:05:05 +0100

    debuging the function that checks the img loaded before animations

--- script.js
+++ script.js
@@ -7,45 +7,55 @@ menu.onclick = () => {
     navbar.classList.toggle('open');
 }
 
-var images = document.querySelectorAll("img");
-var imagesLoaded = 0;
+// check for background-image
+window.onload = function() {
 
-for (var i = 0; i < images.length; i++) {
-  var image = new Image();
-  image.src = images[i].src;
-  image.onload = function() {
-    imagesLoaded++;
-    if (imagesLoaded === images.length) {
+  var images = document.querySelectorAll('.hero');
+  var imagesLoaded = 0;
 
-      // // ANIMATIONS //
-      // trigger the animations when the user scrolls down the page
-      window.onscroll = function() {
-        var scrollPos = window.pageYOffset;
-        
-        if (scrollPos >= 0 && scrollPos < 230) {  // Main Section
-          document.getElementById("profile-pic").className = "hidden";
-          document.getElementById("main-about").className = "hidden";
+  for (var i = 0; i < images.length; i++) {
+    var imageSrc = window.getComputedStyle(images[i], null).backgroundImage;
+    var image = new Image();
+    image.src = imageSrc.slice(4, -1).replace(/"/g, "");
 
-        } else if (scrollPos >= 230 && scrollPos < 460) {  // About Section
-          document.getElementById("profile-pic").className = "fade-in-left";
-          document.getElementById("main-about").className = "about-text fade-in-right";
-          document.getElementById("box-id").className = "hidden";
+    image.onload = function() {
+      imagesLoaded++;
+      if (imagesLoaded === images.length) {
+        // All background-image loaded correctly
+        console.log('Todas las imágenes han cargado correctamente');
 
-        } else if (scrollPos >= 460 && scrollPos < 2100) {  // Web Services Section
-          document.getElementById("box-id").className = "box scale-in-center";
-          document.getElementById("box-skills-1").className = "hidden";
-          document.getElementById("box-skills-2").className = "hidden";
-          document.getElementById("box-skills-3").className = "hidden";
-          document.getElementById("box-skills-4").className = "hidden";
+        document.getElementById("home").className = "hero slide-in-bck-center";
+        document.getElementById("content-id").className = "content text-focus-in";
 
-        } else if (scrollPos >= 2100 && scrollPos < 2920) {  // Skills Section
-          console.log("Skills Section");
-          document.getElementById("box-skills-1").className = "box-skills fade-in-bottom";
-          document.getElementById("box-skills-2").className = "box-skills fade-in-bottom";
-          document.getElementById("box-skills-3").className = "box-skills fade-in-bottom";
-          document.getElementById("box-skills-4").className = "box-skills fade-in-bottom";
-        }
+        // trigger the animations when the user scrolls down the page
+        window.onscroll = function() {
+          var scrollPos = window.pageYOffset;
+
+          if (scrollPos >= 0 && scrollPos < 230) {  // Main Section
+            document.getElementById("profile-pic").className = "hidden";
+            document.getElementById("main-about").className = "hidden";
+
+          } else if (scrollPos >= 230 && scrollPos < 460) {  // About Section
+            document.getElementById("profile-pic").className = "fade-in-left";
+            document.getElementById("main-about").className = "about-text fade-in-right";
+            document.getElementById("box-id").className = "hidden";
+
+          } else if (scrollPos >= 460 && scrollPos < 2100) {  // Web Services Section
+            document.getElementById("box-id").className = "box scale-in-center";
+            document.getElementById("box-skills-1").className = "hidden";
+            document.getElementById("box-skills-2").className = "hidden";
+            document.getElementById("box-skills-3").className = "hidden";
+            document.getElementById("box-skills-4").className = "hidden";
+
+          } else if (scrollPos >= 2100 && scrollPos < 2920) {  // Skills Section
+            console.log("Skills Section");
+            document.getElementById("box-skills-1").className = "box-skills fade-in-bottom";
+            document.getElementById("box-skills-2").className = "box-skills fade-in-bottom";
+            document.getElementById("box-skills-3").className = "box-skills fade-in-bottom";
+            document.getElementById("box-skills-4").className = "box-skills fade-in-bottom";
+          }
+        };
       };
     }
   };
-}
+};
